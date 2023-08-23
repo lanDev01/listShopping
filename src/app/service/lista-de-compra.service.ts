d50ce3e13c4ff26e2846b8395c9fa53310959680
Alan <lnjnr9215@gmail.com>
2023-08-23 13:52:11 -0300

    salvando dados no localStorage

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -6,29 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ListaDeCompraService {
 
-  private listaDeCompra: Item[] = [
-    {
-      "id": 1,
-      "nome": "Queijo prato",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 2,
-      "nome": "Leite integral",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 3,
-      "nome": "Mamão papaia",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": true
-    },
-  ]
+  private listaDeCompra: Item[] = []
 
   constructor() {
-    console.log('Instanciando dependências necessárias para o serviço.');
+    this.listaDeCompra = JSON.parse(localStorage.getItem('itens') || '[]')
   }
 
   getListaDeCompra(){
@@ -46,19 +27,26 @@ export class ListaDeCompraService {
     return item
   }
 
-  adicionarItemLista(nomeItem: string) {
+  adicionarItemLista(nomeItem: string){
     const item = this.criarItem(nomeItem)
     this.listaDeCompra.push(item);
+    // this.atualizarLocalStorage();
   }
 
-  editarItemLista(itemAntigo: Item, nomeEditado: string) {
+  editarItemLista(itemAntigo: Item, nomeEditado: string){
     const itemEditado: Item = {
       id: itemAntigo.id,
       nome: nomeEditado,
       data: itemAntigo.data,
       comprado: itemAntigo.comprado
     }
-    const id = itemAntigo.id
-    this.listaDeCompra.splice(Number(id)-1, 1, itemEditado)
+    const id = itemAntigo.id;
+    this.listaDeCompra.splice(Number(id)-1, 1, itemEditado);
+    // this.atualizarLocalStorage();
   }
+
+  atualizarLocalStorage(){
+    localStorage.setItem('itens', JSON.stringify(this.listaDeCompra));
+  }
+
 }
